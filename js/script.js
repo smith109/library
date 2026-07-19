96eb65674b6263fb4a766b5f08273dcc47ba56c5
smith109 <devon.smith017@gmail.com>
2026-07-19 16:04:01 -0500

    Add a function to display each book on the page

--- js/script.js
+++ js/script.js
@@ -16,3 +16,30 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+function displayBooks() {
+  const bookCards = document.querySelector('.book-cards');
+  bookCards.replaceChildren();
+
+  myLibrary.forEach((book) => {
+    const cardElement = document.createElement('div');
+    const bookTitleElement = document.createElement('h2');
+    const bookAuthorElement = document.createElement('p');
+    const bookPagesElement = document.createElement('p');
+    const bookReadElement = document.createElement('p'); 
+
+    bookTitleElement.textContent = book.title;
+    bookAuthorElement.textContent = `By: ${book.author}`;
+    bookPagesElement.textContent = `${book.pages} pages`;
+    bookReadElement.textContent = book.read ? 'Read' : 'Not read yet';
+
+    cardElement.append(
+      bookTitleElement, 
+      bookAuthorElement, 
+      bookPagesElement, 
+      bookReadElement
+    );
+
+    bookCards.appendChild(cardElement);
+  });
+}
